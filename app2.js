2b7cdd16c67f3ac8ecb79d702df844317cff3504
Emmalauren sambu <eparlaurensanbu@gmail.com>
2018-05-23 16:03:05 +0300

    new files about functions

---
+++ app2.js
@@ -0,0 +1,20 @@
+var a = 300;
+/* var b = 40; */ 
+
+try{
+console.log(a/b)
+
+}
+
+catch(error) {
+console.log(error.message)
+
+}
+
+finally
+{
+console.log('finally done')
+}
+
+console.log('finished')
+console.log('past error')
---
+++ objects.js
@@ -0,0 +1,6 @@
+var Student ={
+	firstName:"Lauren",
+	lastName:"M",
+	surName:"Sambu"
+}
+console.log(Student.firstName);
